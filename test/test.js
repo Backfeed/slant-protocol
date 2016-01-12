22b7690783eb64aee59949467448778786b78adf
Adam Goldman <adamgoldman7@gmail.com>
2016-01-12 16:40:42 +0200

    test - protect scenarios from mutating

--- test/test.js
+++ test/test.js
@@ -8,26 +8,26 @@ describe('evaluate', function() {
   var evaluators = [];
 
   it('should run step 1', function () {
-    var step1 = scenarios["1"];
+    var step1 = getStep(1);
     evaluators = protocol.evaluate(step1);
     expect(evaluators[0].reputation).to.be.equal(0.197676);
   });
 
   it('should run step 2', function () {
-    var step2 = scenarios["2"];
+    var step2 = getStep(2);
     evaluators = protocol.evaluate(step2);
     expect(evaluators[0].reputation).to.be.equal(0.200938);
     expect(evaluators[1].reputation).to.be.equal(0.197686);
   });
 
   it('should run step 3', function () {
-    var step3 = scenarios["3"];
+    var step3 = getStep(3);
     evaluators = protocol.evaluate(step3);
     expect(evaluators[0].reputation).to.be.equal(0.198631);
   });
 
   it('should run step 4', function () {
-    var step4 = scenarios["4"];
+    var step4 = getStep(4);
     evaluators = protocol.evaluate(step4);
     expect(evaluators[0].reputation).to.be.equal(0.205115);
     expect(evaluators[1].reputation).to.be.equal(0.200952);
@@ -35,3 +35,8 @@ describe('evaluate', function() {
   });
 
 });
+
+// Clones the scneraio so it won't mutate with each test run
+function getStep(n) {
+  return JSON.parse(JSON.stringify(scenarios))[n];
+}
